b3e4d42adfa413d9738fe726cdcee071a13748c2
heeseonim <hisundl@naver.com>
2020-04-24 12:37:01 +0900

    크리스마스까지 남은 시간

---
+++ Day05_Date/src/index.js
@@ -0,0 +1,27 @@
+// You're gonna need this
+const NINE_HOURS_MILLISECONDS = 32400000;
+const clock = document.querySelector("#clock");
+
+function getTime() {
+  // Don't delete this.
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const now = new Date();
+  const d_day = xmasDay - now;
+
+  const day = Math.floor(d_day / (1000 * 60 * 60 * 24));
+  const hour = Math.floor((d_day % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minute = Math.floor((d_day % (1000 * 60 * 60)) / (1000 * 60));
+  const second = Math.floor((d_day % (1000 * 60)) / 1000);
+
+  const d = day < 10 ? `0${day}` : `${day}`;
+  const h = hour < 10 ? `0${hour}` : `${hour}`;
+  const m = minute < 10 ? `0${minute}` : `${minute}`;
+  const s = second < 10 ? `0${second}` : `${second}`;
+  clock.innerHTML = `${d}d ${h}h ${m}m ${s}s`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+init();
